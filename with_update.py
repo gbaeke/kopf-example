67901098c2b61e88a55c9cf2630536cbbe10f932
Geert Baeke <geertbaeke@MacBook.local>
2020-01-26 11:39:35 +0100

    update handler

--- with_update.py
+++ with_update.py
@@ -3,10 +3,42 @@ import kubernetes.client
 from kubernetes.client.rest import ApiException
 import yaml
 
+
+    
+
 @kopf.on.create('baeke.info', 'v1', 'demowebs')
-def create_fn(spec, **kwargs):
-    name = kwargs["body"]["metadata"]["name"]
-    print("Name is %s\n" % name)
+def create_fn(spec, name, **kwargs):
+    doc = get_yaml(spec, name, **kwargs)
+    
+    kopf.adopt(doc)
+
+    # Actually create an object by requesting the Kubernetes API.
+    api = kubernetes.client.AppsV1Api()
+    try:
+      depl = api.create_namespaced_deployment(namespace=doc['metadata']['namespace'], body=doc)
+      # Update the parent's status.
+      return {'children': [depl.metadata.uid]}
+    except ApiException as e:
+      print("Exception when calling AppsV1Api->create_namespaced_deployment: %s\n" % e)
+    
+  
+@kopf.on.update('baeke.info', 'v1', 'demowebs')
+def update_fn(spec, name, **kwargs):
+    doc = get_yaml(spec, name, **kwargs)
+    
+    kopf.adopt(doc)
+
+    # Actually patch an object by requesting the Kubernetes API.
+    api = kubernetes.client.AppsV1Api()
+    try:
+      depl = api.patch_namespaced_deployment(name=name+"-deployment", namespace=doc['metadata']['namespace'], body=doc)
+      # Update the parent's status.
+      return {'children': [depl.metadata.uid]}
+    except ApiException as e:
+      print("Exception when calling AppsV1Api->update_namespaced_deployment: %s\n" % e)
+    
+
+def get_yaml(spec, name, **kwargs):
     # Create the deployment spec
     doc = yaml.safe_load(f"""
         apiVersion: apps/v1
@@ -49,16 +81,4 @@ def create_fn(spec, **kwargs):
               - name: workdir
                 emptyDir: {{}}
     """)
-
-    # Make it our child: assign the namespace, name, labels, owner references, etc.
-    kopf.adopt(doc)
-
-    # Actually create an object by requesting the Kubernetes API.
-    api = kubernetes.client.AppsV1Api()
-    try:
-      depl = api.create_namespaced_deployment(namespace=doc['metadata']['namespace'], body=doc)
-      # Update the parent's status.
-      return {'children': [depl.metadata.uid]}
-    except ApiException as e:
-      print("Exception when calling AppsV1Api->create_namespaced_deployment: %s\n" % e)
-      
+    return doc
